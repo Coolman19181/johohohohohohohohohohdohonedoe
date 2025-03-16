5f6f4881888d649ece3b2716971eb1c188d1987f
Coolman19181 <168024766+Coolman19181@users.noreply.github.com>
2025-03-16 12:48:45 -0400

    Update john doe.lua

--- john doe.lua
+++ john doe.lua
@@ -119,5 +119,3 @@ game:GetService("UserInputService").InputBegan:Connect(function(input, gameProce
         playAnimation()
     end
 end)
-
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/Imagnir/r6_anims_for_r15/main/r6
